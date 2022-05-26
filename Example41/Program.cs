93992e5c73d42cd60073e1770e8d5aab414c942f
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-05-26 22:29:51 +0300

    Рещили задачу 41

---
+++ Example41/Program.cs
@@ -0,0 +1,33 @@
+﻿
+/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+void AddNumber(int [] array)
+{
+    int temp=0;
+    try
+    {
+        for (int i = 0; i <= array.Length; i++)
+        {
+            
+            int number = Convert.ToInt32(Console.ReadLine());
+            array[i] = number;
+            if (array[i] > 0)
+            {
+                temp+=1;
+            }
+            Array.Resize (ref array,array.Length+1);
+        }
+    } 
+    catch
+    {
+       Array.Resize (ref array,array.Length-1); 
+    }
+    Console.ForegroundColor=ConsoleColor.Green;
+    Console.WriteLine($"Сумма чиcел > 0 = {temp}");
+    Console.ResetColor();
+}
+int [] numbers= new int[1];
+Console.WriteLine("Введите любое количество чисел:");
+AddNumber(numbers);
